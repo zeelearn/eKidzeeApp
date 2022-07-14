2d5e069ab16bb935cdbc35764188ea5980df5726
Sudhir Patil <sudhir.patil@zeelearn.com>
2022-07-14 17:51:00 +0530

    video player

--- lib/ui/theme.dart
+++ lib/ui/theme.dart
@@ -4,6 +4,23 @@
 
 import 'package:flutter/material.dart';
 
+class AppTheme {
+  static final light = ThemeData(
+    brightness: Brightness.light,
+    colorScheme: const ColorScheme.light(secondary: Colors.red),
+    disabledColor: Colors.grey.shade400,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+
+  static final dark = ThemeData(
+    brightness: Brightness.dark,
+    colorScheme: const ColorScheme.dark(secondary: Colors.red),
+    disabledColor: Colors.grey.shade400,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+}
+
+
 class AppSizes {
   static const int splashScreenTitleFontSize = 48;
   static const int titleFontSize = 34;
